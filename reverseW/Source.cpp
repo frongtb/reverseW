1c13063f56350492537b70465080aa29d0291b34
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-11-21 16:01:52 +0700

    Add project files.

---
+++ reverseW/Source.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+char* reverse(char *);
+int main(int argc,char *argv[])
+{
+	char check;
+	int i;
+	char str[80];
+	char *str_ptr;
+	printf_s("Enter String : ");
+	gets_s(str);
+	str_ptr = reverse(str);
+	printf_s("\nReverse string is ");
+	puts(str_ptr);
+	_getch();
+
+	printf_s("\nDo you want to reverse word again\n");
+	printf_s("Yes(press Y) or No(press any key) : ");
+	check = _getche();
+	if (check == 'Y')
+	{
+		while (check != 'E')
+		{
+			printf_s("\nEnter String : ");
+			gets_s(str);
+			str_ptr = reverse(str);
+			printf_s("\nReverse string is ");
+			puts(str_ptr);
+			_getch();
+			printf_s("Continue(press any key) or Exit(press E) : ");
+			check = _getche();
+		}
+	}
+}
+
+char* reverse(char* string)
+{
+	char ch;
+	int x, y;
+	for (x=0,y=strlen(string)-1;x<y;x++,y--)
+	{
+		ch = *(string + y);
+		*(string + y) = *(string + x);
+		*(string + x) = ch;
+	}
+	return string;
+}
+
